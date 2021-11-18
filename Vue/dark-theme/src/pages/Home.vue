8a894ec5e232b327c7d1b7f8900084fa106c00f2
Gustavo Silva <gustavospr@hotmail.com>
2021-11-18 13:35:28 -0300

    Sistema de Rotas funcionando

---
+++ Vue/dark-theme/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data(){
+        return {
+            msg: 'Estou funcionando.'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
